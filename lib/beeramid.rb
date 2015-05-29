cf483f89b190dcee7cf020ab9a6cb2bd70eb11dd
Mario Finelli <mario@finel.li>
2015-05-29 11:22:01 -0400

    Add buy beer method and tests

--- lib/beeramid.rb
+++ lib/beeramid.rb
@@ -1,5 +1,16 @@
 class Beeramid
 
+  attr_reader :price, :amount
+
+  def initialize(params={})
+    @price = params.fetch(:price, 5.0).to_f
+    @amount = params.fetch(:amount, 50.0).to_f
+  end
+
+  def buy_beer
+    (amount/price).to_i
+  end
+
   def self.calculate(amount, price=5)
     if price <= 0
       return nil
--- spec/beeramid_spec.rb
+++ spec/beeramid_spec.rb
@@ -1,7 +1,68 @@
 require 'beeramid'
 
 describe Beeramid do
-  describe '.calculate' do
+
+  describe '#initialize' do
+    context 'with no parameters' do
+      b = Beeramid.new
+
+      it 'should return five for price' do
+        expect(b.price).to eql(5.0)
+      end
+
+      it 'should return fifty for amount' do
+        expect(b.amount).to eql(50.0)
+      end
+    end
+
+    context 'with integer options' do
+      b=Beeramid.new(amount: 10, price: 2)
+
+      it 'should return float for price' do
+        expect(b.price).to eql(2.0)
+      end
+
+      it 'should return float for amount' do
+        expect(b.amount).to eql(10.0)
+      end
+    end
+  end
+
+  describe '#buy_beer' do
+    context 'with $20 at $5/can' do
+      b = Beeramid.new(amount: 20)
+
+      it 'should buy four cans' do
+        expect(b.buy_beer).to eql(4)
+      end
+    end
+
+    context 'with $13 at $5/can' do
+      b = Beeramid.new(amount: 13)
+
+      it 'should buy two cans' do
+        expect(b.buy_beer).to eql(2)
+      end
+    end
+
+    context 'with $12.75 at $1/can' do
+      b = Beeramid.new(amount: 12.75, price: 1)
+
+      it 'should buy 12 cans' do
+        expect(b.buy_beer).to eql(12)
+      end
+    end
+
+    context 'with $60 at $5.62/can' do
+      b = Beeramid.new(amount: 60, price: 5.63)
+
+      it 'should buy 10 cans' do
+        expect(b.buy_beer).to eql(10)
+      end
+    end
+  end
+
+  describe '#calculate' do
 
     context 'with negative amount' do
       it 'returns zero' do
