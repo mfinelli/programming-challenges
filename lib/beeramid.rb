e91726927fc68ee631b391fd6ed0bf1986d3d86b
Mario Finelli <mario@finel.li>
2015-05-30 20:43:10 -0400

    Comment the calculate method

--- lib/beeramid.rb
+++ lib/beeramid.rb
@@ -16,13 +16,20 @@ class Beeramid
       return nil
     end
 
+    # We should return no levels if we don't have any money or not enough
+    # money to buy any beer!
     if amount <= 0 or amount < price
       return 0
     end
 
+    # Calculate how much beer we can buy and initialize the number of levels
+    # to zero.
     cans_remaining = buy_beer
     levels = 0
 
+    # Each n level requires n beers, so while we have beers left and they are
+    # more than the next level's cost (n + 1) then we can add a level.
+    # Otherwise we have reached the maximum number of levels.
     while cans_remaining > 0
       if cans_remaining < levels + 1
         return levels
